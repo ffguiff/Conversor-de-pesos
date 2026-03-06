3a6f89244af20b3aff57030b9e4e08fcf483efe1
Guilherme Ferreira <90099945+ffguiff@users.noreply.github.com>
2026-03-05 21:35:18 -0300

    adição de novos metodos a class
    adição da função de conveção de peso de kg para ml do álcool;
    criação de um historico que registra o resultado da ultima transação realizada;
    tratamento de erros que poderiam ocorrer com o uso de try/ except;
    criação de função pra auxiliar na devolução alterando kg para und para as capsulas;
    criação de função que retorna ultimo valor registrado no historico.

--- pooCapsula.py
+++ pooCapsula.py
@@ -3,25 +3,31 @@ class Capsula:
     def calculoComValordaCapsula(self):
         while True:
             print("DIGITE TODAS AS INFORMAÇÕES SEM CARACTER!")
-            pesoCapsula = input("Digite o peso da capsula: ")
+            pesoCapsula = input("Digite o peso da capsula(ex: 98,0 mg = 98): ")
             quantidadeEmUnd = input("Digite a quantidade de unidades que deseja converter: ")
             try:
                 intPesoCapsula = int(pesoCapsula)
                 intQuantidadeEmUnd = int(quantidadeEmUnd)
-                valorEmKg = (intPesoCapsula * intQuantidadeEmUnd)/1000
 
+                valorEmKg = (intPesoCapsula * intQuantidadeEmUnd)/1000
                 resultado = f"{valorEmKg:,.3f}".replace(".", ",")
-                print("\nvocê devera pesar: ",resultado, "mg", "\nque será o mesmo que: ", quantidadeEmUnd, "und")
+
+                linha = f"você deverá pesar: {resultado} mg, que será o mesmo que: {quantidadeEmUnd} und\n"
+                print(linha)
+
+                arqHistorico = open("historico.txt","a")
+                arqHistorico.write(linha)
+                arqHistorico.close()
 
                 break
             except ValueError:
 
-                print("ERRO!")
+                print("ERRO!VOCÊ USOU '.' OU ','")
 
     def calculoSemPesoDaCapsula(self):
         while True:
             print("DIGITE TODAS AS INFORMAÇÕES SEM CARACTER!")
-            pesoDaCaixa = input("Digite o peso da caixa em mg: ")
+            pesoDaCaixa = input("Digite o peso da caixa em mg(ex: 12kg = 12000): ")
             quantidadeDecapsulasDentroDaCaixa = input("Digite a quantidade de capsulas desta Caixa: ")
             quantidadeEmUnd = input("Digite a quantidade de unidades que deseja converter: ")
 
@@ -35,11 +41,73 @@ class Capsula:
 
                 resultado = f"{valorEmKg:,.3f}".replace(".", ",")
 
-                print("O peso da capsula é de: ", pesoCapsula, "mg")
-                print("\nvocê devera pesar: ",resultado, "mg", "\nque será o mesmo que: ", quantidadeEmUnd, "und")
+                linha = f"você devera pesar: {resultado} mg que será o mesmo que: {quantidadeEmUnd} und. O peso da capsula é de: {round(pesoCapsula,3)} mg"
+
+                print(linha)
+
+                arqHistorico = open("historico.txt","a")
+                arqHistorico.write(linha)
+                arqHistorico.close()
 
                 break
 
             except ValueError: 
 
-                print("ERRO!")
+                print("ERRO!VOCÊ USOU '.' OU ','")
+
+    def convercaoDePesoAlcool(self):
+        pesorequerido = float(input("Digite o peso que em ml que é requisitado na op ex(21.000):"))
+        calculoDeConvercao = round(pesorequerido * 0.808)
+
+        linha = f"O peso em kg de {pesorequerido} é de {calculoDeConvercao} kg"
+        print(linha)
+
+        arqHistorico = open("historico.txt","a")
+        arqHistorico.write(linha)
+        arqHistorico.close()
+
+    def historico(self):
+        with open("historico.txt", "r") as arquivo:
+            # 1. Pega todas as linhas e remove a última se estiver vazia
+            linhas = [linha.strip() for linha in arquivo if linha.strip()]
+            
+            if linhas:
+                # 2. Pega a última linha do arquivo
+                ultima_linha = linhas[-1]
+                
+                # 3. Divide a linha pelo ';' e pega o último elemento da lista (índice -1)
+                partes = ultima_linha.split(';')
+                ultimo_valor = partes[-1].strip()
+                
+                print(f"O último valor salvo é: {ultimo_valor}")
+            else:
+                print("O arquivo está vazio.")
+
+    def devolucaoCapsulas(self):
+        while True:
+            print("DIGITE TODAS AS INFORMAÇÕES SEM CARACTER!")
+            peso_da_capsula =  input("digite o peso da capsula: ")
+            peso_a_devolver = input("digite o peso que deseja converter para und: ")
+
+            try:
+                intPesoDaCapsula= int(peso_da_capsula)
+                intPesoaDevolver = int(peso_a_devolver)
+                devolver = intPesoaDevolver / intPesoDaCapsula
+
+                resultado = f"{devolver:,.3f}".replace(".", ".")
+
+                print(f"{intPesoaDevolver}kg será {resultado} und")
+
+                break
+
+            except ValueError:
+
+                 print("ERRO!VOCÊ USOU '.' OU ','")   
+
+
+
+        
+
+
+#inserir campo de calculo para devolução e pre salvar ultimos pesos de capsulas caso o usuario 
+#queira relembrar o peso delas
